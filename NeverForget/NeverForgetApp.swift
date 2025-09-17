f0fcb53082df827963b4e180d68bd4f74bb3e2f3
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-09-17 16:04:01 +0200

    Initial Commit

---
+++ NeverForget/NeverForgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeverForgetApp.swift
+//  NeverForget
+//
+//  Created by Eric on 17/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct NeverForgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
